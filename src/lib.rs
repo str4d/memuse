da115cb7a55e66d3c8819e50e4201f434060f045
Jack Grigg <thestr4d@gmail.com>
2021-09-03 20:57:21 +0100

    Add support for &str, String, Option<T>, and &[T]

--- src/lib.rs
+++ src/lib.rs
@@ -30,14 +30,38 @@ macro_rules! impl_no_dynamic_usage {
 impl_no_dynamic_usage!(i8, i16, i32, i64, i128, isize);
 impl_no_dynamic_usage!(u8, u16, u32, u64, u128, usize);
 impl_no_dynamic_usage!(f32, f64, char, bool);
+impl_no_dynamic_usage!(&str);
+
+impl DynamicUsage for String {
+    fn dynamic_usage(&self) -> usize {
+        self.capacity()
+    }
+}
+
+impl<T> DynamicUsage for Option<T>
+where
+    T: DynamicUsage,
+{
+    fn dynamic_usage(&self) -> usize {
+        self.as_ref().map(DynamicUsage::dynamic_usage).unwrap_or(0)
+    }
+}
+
+impl<T> DynamicUsage for &[T]
+where
+    T: DynamicUsage,
+{
+    fn dynamic_usage(&self) -> usize {
+        self.iter().map(DynamicUsage::dynamic_usage).sum::<usize>()
+    }
+}
 
 impl<T> DynamicUsage for Vec<T>
 where
     T: DynamicUsage,
 {
     fn dynamic_usage(&self) -> usize {
-        self.capacity() * mem::size_of::<T>()
-            + self.iter().map(DynamicUsage::dynamic_usage).sum::<usize>()
+        self.capacity() * mem::size_of::<T>() + self.as_slice().dynamic_usage()
     }
 }
 
@@ -62,6 +86,21 @@ mod tests {
         assert_eq!(129u8.dynamic_usage(), 0);
         assert_eq!(3i128.dynamic_usage(), 0);
         assert_eq!(7.0f32.dynamic_usage(), 0);
+        assert_eq!("foobar".dynamic_usage(), 0);
+    }
+
+    #[test]
+    fn string() {
+        assert_eq!(String::new().dynamic_usage(), 0);
+        assert_eq!("foobar".to_string().dynamic_usage(), 6);
+    }
+
+    #[test]
+    fn option() {
+        let a: Option<Vec<u8>> = None;
+        let b: Option<Vec<u8>> = Some(vec![7u8; 4]);
+        assert_eq!(a.dynamic_usage(), 0);
+        assert_eq!(b.dynamic_usage(), 4);
     }
 
     #[test]
